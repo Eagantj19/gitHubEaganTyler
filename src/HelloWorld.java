4e6f74ca73678784186e05549e054decae4dc473
nwallace23 <100313337+nwallace23@users.noreply.github.com>
2022-02-25 14:21:22 -0500

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Spring break gonna kick my ass, i am going to sweat night a day.");
+        System.out.println("Spring break gonna kick my ass, i am going to sweat night and day.");
     }
 }
